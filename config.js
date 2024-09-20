296f0d9de41249884f0a7198e69fb4af462ad19a
itsme-didularashmika <bandaraakarsha99@gmail.com>
2024-09-20 15:23:49 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "bI8kVTyI#ct5gxS-Lf6d5Ml-nkU4UmwmiQtFdCGJVqKas03xjnk8",
+SESSION_ID: process.env.SESSION_ID || "VCVmEKDT#9w7i8hZvDA5fI8Tdhl9xuVWOISmzglFp-qOSD7G3tY8",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
